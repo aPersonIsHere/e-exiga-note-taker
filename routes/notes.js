e908278e980d581ed67a0d0fa530b8515f2bef84
Emmanuel Exiga <33707404+aPersonIsHere@users.noreply.github.com>
2023-10-27 23:09:40 -0500

    This adds the necessary files and glue to make the html, css, and js work to load up when a user accesses the site. Unfortunately, there is a strange error that is preventing the person from actually clicking on a note and view the title and message in the center of the webpage.

---
+++ routes/notes.js
@@ -0,0 +1,50 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+//Import the UUID npm package from https://www.npmjs.com/package/uuid to generate a unique ID for each message
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id === noteId);
+            return result.length > 0 ? res.json(result) : res.json('No note with that ID');
+        });
+});
+
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id !== noteId);
+  
+            writeToFile('./db/db.json', result);
+  
+            res.json(`Item ${noteId} has been deleted`);
+        });
+});
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+module.exports = notes;
